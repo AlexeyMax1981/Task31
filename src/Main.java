50b398589d9dfa12e7323fb6725ac3adb214cf6b
Alexey 1981 <maximenko.av@rambler.ru>
2023-09-08 08:20:55 +0300

    Add

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println("Ваш бонус составляет " + bonus + " миль.");
+    }
+}
